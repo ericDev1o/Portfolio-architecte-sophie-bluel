3fda15c501dda536cf2fbfd4c19f97f9065cf472
Eric Faraut <eric.faraut@hotmail.fr>
2025-03-24 14:12:05 +0100

    backend debug console.log are removed.

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -3,12 +3,9 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(' ')[1]
-		console.log("jwt.verify(token, process.env.TOKEN_SECRET): " + jwt.verify(token, process.env.TOKEN_SECRET));
 		const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-		console.log("decodedToken: " + decodedToken);
 		const userId = decodedToken.userId
 		req.auth = { userId }
-		console.log("req.auth.userId: " + req.auth.userId);
 
 		if (req.body.userId && req.body.userId !== userId) {
 			throw 'Invalid user ID'
--- Backend/middlewares/multer-config.js
+++ Backend/middlewares/multer-config.js
@@ -11,16 +11,12 @@ const storage = multer.diskStorage({
 		callback(null, './images')
 	},
 	filename:  (req, file, callback) => {
-		console.log("enter multer filename")
 		const filename = file.originalname.split(' ').join('_')
-		console.log("filename : " + filename)
 		const filenameArray = filename.split('.')
 		filenameArray.pop()
 		const filenameWithoutExtension = filenameArray.join('.')
-		console.log("filenameWithoutExtension : " + filenameWithoutExtension)
 		const extension = MIME_TYPE[file.mimetype]
 		callback(null, filenameWithoutExtension + Date.now() + '.' + extension)
-		console.log("exit multer filename")
 	}
 })
 
